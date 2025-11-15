855e1e50d44a40706295f1640eb5ec068ea5d001
abdurrnu <nuh.abdur.rahman.5@gmail.com>
2025-11-15 04:26:01 -0600

    updated level for alpha submission

---
+++ Tiled/sewer-tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="sewer-tileset" tilewidth="16" tileheight="16" tilecount="420" columns="14">
+ <image source="../../../../../../Downloads/Legacy Fantasy - Sewer Canals/Assets/Tiles.png" trans="000000" width="224" height="480"/>
+</tileset>
